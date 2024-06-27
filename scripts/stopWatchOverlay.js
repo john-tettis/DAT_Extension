96d555b58d5fffe210dca8cc03a2c316a58fa663
John Tettis <johndavidtettis@gmail.com>
2024-06-27 18:07:50 -0400

    added styles for the stopwatch

--- scripts/stopWatchOverlay.js
+++ scripts/stopWatchOverlay.js
@@ -19,9 +19,26 @@ const html = `
 </div>
 `
 const container = document.createElement('div')
+container.classList.add('stopwatch-container')
 //add styles here to fix to screen
+const styles = document.createElement('style')
+// Apply CSS rules to the style element
+styles.textContent = `
+    .stopwatch-container{
+        position: fixed;
+        right:10px;
+        top: 50px;
+        width: 100px; /* Set width as needed */
+        height: 100px; /* Set height as needed */
+        background-color: blue; /* Set background color */
+    }
+    .timer{
+        position: absolute;
+    }
+`;
 //add logic to update time
-const style = document
+//put the stopwatch display inside the container
 container.innerHTML = html;
-console.log(container)
+console.log(container);
 document.body.appendChild(container);
+document.body.appendChild(styles);
